f262374d5bfa1f8116988ef383a36da253bedabc
Mong'are <Mongaresams@gmail.com>
2022-01-22 00:43:36 +0300

    Update the delete functionality code

--- src/redux/books/api.js
+++ src/redux/books/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { booksFetched, bookAdded, bookRemoved } from './books';
 
 export const getBooks = () => async (dispatch) => {
-  axios.get('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/wRwCMlysDq3OKY2pOQRB/books')
+  axios.get('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/QiDluot0X8BBAUYhtRYg/books')
     .then((response) => {
       const newBooks = response.data;
       const mappedBooks = Object.entries(newBooks).map(([id, book]) => {
@@ -22,17 +22,15 @@ export const sendBookToApi = (payload) => async (dispatch) => {
     title,
   };
 
-  await axios.post('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/wRwCMlysDq3OKY2pOQRB/books', newBook);
+  await axios.post('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/QiDluot0X8BBAUYhtRYg/books', newBook);
 
   dispatch(bookAdded(payload));
 };
 
 export const deleteBook = (payload) => async (dispatch) => {
-  const newBook = {
-    item_id: payload,
-  };
+  const newBook = payload;
 
-  await axios.delete(`https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/wRwCMlysDq3OKY2pOQRB/books/${newBook}`);
+  await axios.delete(`https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/QiDluot0X8BBAUYhtRYg/books/${newBook}`);
 
   dispatch(bookRemoved(payload));
 };
